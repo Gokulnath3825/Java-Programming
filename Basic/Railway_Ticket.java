39801e89fe904d6ed7d515a85edccdc928a4a7e9
Gokulnath <gokulj172@gmail.com>
2026-02-15 23:28:14 +0530

    Add files via upload
    solved 16 problem

---
+++ Basic/Railway_Ticket.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Railway_Ticket {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int genPass=sc.nextInt();
+        int genFare=sc.nextInt();
+        int acPass=sc.nextInt();
+        int acFare=sc.nextInt();
+        int platform=sc.nextInt();
+        int maintenance=sc.nextInt();
+        int fuel=sc.nextInt();
+
+        int Revenue =(genPass *genFare)+ (acPass * acFare)+platform-maintenance-fuel;
+
+        System.out.println("Net Railway Revenue ="+Revenue);
+        sc.close();
+    }
+    
+}
