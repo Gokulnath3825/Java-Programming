66508b5c3e280aa806e0573bb0b27ba8022712ee
Gokulnath <gokulj172@gmail.com>
2026-03-03 14:06:10 +0530

    Add files via upload
    Solved 05 problem

---
+++ While Loop/Problem05.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class Problem05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int nn = n;
+        int rev = 0;
+
+        while(n!=0){
+            int digit = n%10;
+            rev = rev*10+digit;
+            n/=10;
+        }
+        System.out.println("Original: "+nn);
+        System.out.println("Reversed: "+rev);
+        sc.close();
+        sc.close();
+    }
+}
