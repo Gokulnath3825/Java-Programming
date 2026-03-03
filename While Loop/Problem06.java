2117c10a29da95c5c59a16ed4b90aac1ef6facff
Gokulnath <gokulj172@gmail.com>
2026-03-03 14:29:54 +0530

    Add files via upload
    Solved 06 problem

---
+++ While Loop/Problem06.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class Problem06 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int balance = sc.nextInt();
+        int amount=0;
+        boolean ok = true;
+        do {
+            int withdrawal = sc.nextInt();
+            System.out.println("\nEnter withdrawal amount: "+withdrawal);
+            
+            amount = balance - withdrawal;
+            if(withdrawal<=100){
+                System.out.println("Invalid amount! Try again.");
+            }
+            else if(amount<0){
+                System.out.println("Insufficient funds! Try again.");
+            }
+            else{
+                System.out.println("Withdrawal successful! Remaining balance:"+amount);
+                ok=false;
+            }
+            
+        } while (ok);
+    }
+}
