28c4f85f2f02ded9bc531edad96d08cf39e58ecb
Gokulnath <gokulj172@gmail.com>
2026-03-05 12:17:22 +0530

    Add files via upload
    Solved 01 problem

---
+++ Nested Loop/Problem01.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Problem01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+        int column = sc.nextInt();
+
+        for(int i=1;i<=row;i++){
+            if(i==1){
+                for(int l=1;l<=row;l++){
+                    System.out.print(l+" ");
+                }
+                System.out.println();
+            }
+            for(int k=0;k<1;k++){
+                System.out.print(i+" ");
+            }
+            for(int j=1;j<=column;j++){
+                System.out.print(j*i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
