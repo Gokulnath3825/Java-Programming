3901d30dc050a91b4c1633a307982e5b8773eed5
Gokulnath <gokulj172@gmail.com>
2026-03-05 10:07:13 +0530

    Add files via upload
    Solved 25 problem

---
+++ While Loop/Problem25.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Problem25 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i=1;
+        do{
+            int j=0;
+            do {
+                System.out.print(i+" ");
+                j++;
+            } while (i>j);
+            i++;
+            System.out.println();
+        }while(i<=n);
+        sc.close();
+    }
+}
