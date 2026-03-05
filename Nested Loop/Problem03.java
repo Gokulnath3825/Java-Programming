50221cff79bb85857a2a46dc7a48588a759ac5df
Gokulnath <gokulj172@gmail.com>
2026-03-05 13:35:35 +0530

    Add files via upload
    Solved 03 problem

---
+++ Nested Loop/Problem03.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Problem03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String ch = sc.next();
+
+        for(int i=1;i<=n;i++){
+            if(ch.equals("star")){
+                for(int j=1;j<=i;j++){
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+            else if(ch.equals("number")){
+                for(int j=1;j<=i;j++){
+                    System.out.print(j);
+                }
+                System.out.println();
+                
+            }
+        }
+    }
+}
