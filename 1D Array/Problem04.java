95b2d0b890f4677499526443b21d34f367775f11
Gokulnath <gokulj172@gmail.com>
2026-03-08 23:17:39 +0530

    Add files via upload
    Solved 04 problem

---
+++ 1D Array/Problem04.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Problem04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        double[] temp = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            temp[i] = sc.nextDouble();
+        }
+        double max = temp[0];
+        double min = temp[0];
+        double sum = 0;
+        for (int i = 0; i < n; i++) {
+            if (temp[i]>max) {
+                max=temp[i];
+            }
+            if (temp[i] < min) {
+                min = temp[i];
+            }
+            sum += temp[i];
+        }
+
+        double range = max-min;
+        double avg = sum/n;
+
+        System.out.println("\nNumber of Readings: " + n);
+        System.out.printf("Maximum Temperature: %.2f°C\n", max);
+        System.out.printf("Minimum Temperature: %.2f°C\n", min);
+        System.out.printf("Temperature Range: %.2f°C\n", range);
+        System.out.printf("Average Temperature: %.2f°C\n", avg);
+
+        sc.close();
+    }
+}
