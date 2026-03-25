e29d665953da84ed6ab67a00a9a149c3aa62b934
Gokulnath <gokulj172@gmail.com>
2026-03-25 09:52:55 +0530

    Added

--- 1D Array/Problem01.java
+++ 1D Array/Problem01.java
@@ -3,10 +3,10 @@ import java.util.*;
 
 public class Problem01 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);
         int n = sc.nextInt();
 
-        double [] arr = new double[n];
+        double []arr=new double[n];
         double total = 0;
         for(int i=0;i<n;i++){
             arr[i] = sc.nextDouble();
