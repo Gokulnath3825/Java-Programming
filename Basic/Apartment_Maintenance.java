998705933d2908de972bff0217738721e7fd4379
Gokulnath <gokulj172@gmail.com>
2026-04-03 20:59:52 +0530

    Fix formatting and spacing in Apartment_Maintenance.java

--- Basic/Apartment_Maintenance.java
+++ Basic/Apartment_Maintenance.java
@@ -4,15 +4,15 @@ public class Apartment_Maintenance {
         public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int maintenance=sc.nextInt();
+        int maintenance = sc.nextInt();
         int parking=sc.nextInt();
         int hall=sc.nextInt();
         int security=sc.nextInt();
         int electricity=sc.nextInt();
         int cleaning=sc.nextInt();
 
-        int Balance =maintenance+parking+hall-security-electricity-cleaning;
-
+        int Balance = maintenance + parking+hall - security - electricity - cleaning;
+ 
 
         System.out.println("Remaining Balance = "+Balance);
         sc.close();
