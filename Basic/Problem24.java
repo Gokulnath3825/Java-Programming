836f16b5945e2ef7ced737ba8790559632d78e89
Gokulnath <gokulj172@gmail.com>
2026-04-03 21:03:40 +0530

    Add files via upload

---
+++ Basic/Problem24.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Problem24{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int[][] pascal = new int[N][];
+        for (int i = 0; i < N; i++) {
+            pascal[i] = new int[i + 1];
+
+            pascal[i][0] = 1;
+            pascal[i][i] = 1;
+            for (int j = 1; j < i; j++) {
+                pascal[i][j] = pascal[i - 1][j - 1] + pascal[i - 1][j];
+            }
+        }
+
+        System.out.println("Pascal's Triangle (" + N + " rows):");
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < pascal[i].length; j++) {
+                System.out.print(pascal[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
