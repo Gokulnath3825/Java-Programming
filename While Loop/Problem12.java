b24c2751a637ae17b2b78d78a6be93ed8ada5437
Gokulnath <gokulj172@gmail.com>
2026-04-07 22:51:58 +0530

    Refactor palindrome check to use variable 'a'

--- While Loop/Problem12.java
+++ While Loop/Problem12.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 public class Problem12 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int nn = n;
+        int a = sc.nextInt();
+        int nn = a;
         int rev = 0;
 
-        while(n!=0){
-            int digit = n%10;
+        while(a!=0){
+            int digit = a%10;
             rev = rev * 10 + digit;
-            n/=10;
+            a/=10;
         }
 
         if(nn == rev){
