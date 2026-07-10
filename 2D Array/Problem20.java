daa275ed8e8d72a86c4efa107a9a8e2c6fcac755
Gokulnath <gokulj172@gmail.com>
2026-07-10 22:43:01 +0530

    Update Problem20.java

--- 2D Array/Problem20.java
+++ 2D Array/Problem20.java
@@ -6,7 +6,7 @@ public class Problem20 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int [][]matrix = new int[n][n];
-        for(int i=0;i<n;i++){
+        for(int i=0;i < n;i++){
             for(int j=0;j<n;j++){
                 matrix[i][j]=sc.nextInt();
             }
