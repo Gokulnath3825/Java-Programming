d9491a0b50508042d6b3edd749b962dee195c240
Gokulnath <gokulj172@gmail.com>
2026-08-22 05:58:38 +0530

    Update Problem01.java

--- While Loop/Problem01.java
+++ While Loop/Problem01.java
@@ -1,21 +1,30 @@
 
 import java.util.Scanner;
-
 public class Problem01 {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         String correctpassword = sc.nextLine();
+
         int maxAttempt = 3;
+
         int attempt = 0;
+
         boolean login =false;
 
+
         while(attempt<maxAttempt && !login){
+
             System.out.println("\nEnter password:");
+
             String password = sc.nextLine();
+
             attempt++;
+
             if(password.equals(correctpassword)){
+
                 System.out.println("Access granted!");
+
                 login = true;
             }
             else{
