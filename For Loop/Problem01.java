11b38da73e6e20c2ad390311421238699c530e02
Gokulnath <gokulj172@gmail.com>
2026-08-31 20:56:01 +0530

    Update Problem01.java

--- For Loop/Problem01.java
+++ For Loop/Problem01.java
@@ -8,37 +8,55 @@ public class Problem01 {
 
         for(int i = 0; i < n; i++){
             String employeeName = sc.next();
+
             double baseSalary = sc.nextDouble();
+
             int performanceRating = sc.nextInt();
+
             int increment = 0;
 
             if(performanceRating == 5){
                 increment = 15;
+
             } else if(performanceRating == 4){
                 increment = 10;
+
             } else if(performanceRating == 3){
                 increment = 5;
+
             } else if(performanceRating == 2){
                 increment = 2;
+
             } else if(performanceRating == 1){
                 increment = 0;
+
             }
 
             double finalSalary = baseSalary * (1 + increment / 100.0);
+
             totalSum += finalSalary;
 
             System.out.println();
+
             System.out.println("Employee: " + employeeName);
+
             System.out.println("Base Salary: $" + baseSalary);
+
             System.out.println("Performance Rating: " + performanceRating);
+
             System.out.println("Increment: " + increment + "%");
+
             System.out.println("Final Salary: $" + String.format("%.2f",finalSalary));
+
         }
 
         double avg = totalSum / n;
 
         System.out.println("Total Employees Processed: " + n);
+
         System.out.println("Total Payroll: $" +String.format("%.2f",totalSum));
+
         System.out.println("Average Payroll: $" + String.format("%.2f",avg));
+
     }
 }
