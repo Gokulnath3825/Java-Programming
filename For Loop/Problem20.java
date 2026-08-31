c7cf5931ba6dce09c89be622fc432ddb95834f8c
Gokulnath <gokulj172@gmail.com>
2026-08-31 20:52:39 +0530

    Update Problem20.java

--- For Loop/Problem20.java
+++ For Loop/Problem20.java
@@ -4,23 +4,36 @@ import java.util.Scanner;
 public class Problem20 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
+
         double totalDeposit=0;
+
         double totalWithdrawal=0;
+
         double totalTransfer=0;
+
         double totalPayment=0;
+
         for(int i=1;i<=n;i++){
+
             String transactionType = sc.next();
+
             double amount = sc.nextDouble();
+
             String Categorize="";
+
             if(transactionType.equals("Deposit")){
                 Categorize="Credit";
+
                 totalDeposit+=amount;
+
             }
             else{
                 Categorize="Withdrawal";
+
                 if(transactionType.equals("Withdrawal")){
-                    totalWithdrawal+=amount;
+totalWithdrawal+=amount;
                 }
                 if(transactionType.equals("Transfer")){
                     totalTransfer+=amount;
