8e81d796b9d732a821dfc3635466803f94b35910
Gokulnath <gokulj172@gmail.com>
2026-09-17 23:17:35 +0530

    Update Airline_Cargo.java

--- Basic/Airline_Cargo.java
+++ Basic/Airline_Cargo.java
@@ -1,9 +1,7 @@
 import java.util.Scanner;
-
 public class Airline_Cargo {
         public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int weight=sc.nextInt();
         int rate=sc.nextInt();
         int priority=sc.nextInt();
