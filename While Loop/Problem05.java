df6f6e0d5f90f2c4caeabedceaa3419c4b355b43
Gokulnath <gokulj172@gmail.com>
2026-09-17 23:15:30 +0530

    Update Problem05.java

--- While Loop/Problem05.java
+++ While Loop/Problem05.java
@@ -1,13 +1,12 @@
 
 import java.util.Scanner;
-
 public class Problem05 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
         int nn = n;
         int rev = 0;
-
         while(n!=0){
             int digit = n%10;
             rev = rev * 10 + digit;
