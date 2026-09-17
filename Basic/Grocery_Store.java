cae88d59bcf10e813b9344fb421c403a84ffbc03
Gokulnath <gokulj172@gmail.com>
2026-09-17 23:17:56 +0530

    Update Grocery_Store.java

--- Basic/Grocery_Store.java
+++ Basic/Grocery_Store.java
@@ -2,14 +2,11 @@ import java.util.Scanner;
 public class Grocery_Store {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int sales=sc.nextInt();
         int discount=sc.nextInt();
         int gst=sc.nextInt();
-
         int totaldis=(sales*discount)/100;
         float gstAmount=((sales-totaldis)*gst)/100.0f;
-
         float Bill=(sales-totaldis)+gstAmount;
         System.out.println(Bill);
         sc.close();
